14aa78c0d068e6510597c38b6bef3252e0335e33
Netramm <14896015+Netramm@users.noreply.github.com>
2024-06-13 13:20:53 +0200

    Intergrate .yaml

--- drl_agent/src/trainDRLAlgorithm.py
+++ drl_agent/src/trainDRLAlgorithm.py
@@ -8,23 +8,26 @@ import rospy
 from gym import wrappers
 from openai_ros.openai_ros_common import StartOpenAI_ROS_Environment
 from stable_baselines3 import DQN, PPO
+
 import wandb
+from wandb.integration.sb3 import WandbCallback
 
 
 def main():
-    loadModel = False
-    continueTraining = False
-    saveModel = True
-    use_wandb = True
-    #DQN,PPO
-    algorithm ="DQN"
+    loadModel = rospy.get_param('/turtlebot3/load_model')
+    continueTraining = rospy.get_param('/turtlebot3/continueTraining')
+    saveModel = rospy.get_param('/turtlebot3/saveModel')
+    use_wandb = rospy.get_param('/turtlebot3/use_wandb')
+    
     config = {
+        "algorithm": rospy.get_param('/turtlebot3/algorithm'),
         "policy_type": "MlpPolicy",
-        "total_timesteps": 20000,
-        "n_steps_before_every_PPO_update": 500
+        "total_timesteps": rospy.get_param('/turtlebot3/total_timesteps'),
+        "n_steps_before_every_PPO_update": rospy.get_param('/turtlebot3/n_steps_before_every_PPO_update')
     }
+    
     if use_wandb:
-    # Set up W&B
+        # Set up W&B
         run = wandb.init(
             project="DLLab",
             config=config,
@@ -33,33 +36,31 @@ def main():
             # save_code=True,  # optional
         )
 
-
-    env, modelPath = init(algorithm)
+    env, modelPath = init(config["algorithm"])
 
     if (loadModel):
         rospy.logwarn("Loading Model...")
-        model = loadModel(algorithm, modelPath)
+        model = loadModel(config["algorithm"], modelPath)
         inited = False
     else:
         if (continueTraining):
             rospy.logwarn("Continue training")
-            model = loadModel(algorithm, modelPath)
+            model = loadModel(config["algorithm"], modelPath)
         else:
             #model = DQN('MlpPolicy', env, verbose=1)
-            model = startModel(algorithm, env, run, config)
-        
-            model.learn(total_timesteps=200000)
+            model = startModel(config["algorithm"], env, run, config)
         
         if use_wandb:
             model.learn(total_timesteps=config["total_timesteps"],
                         callback=WandbCallback(
-                            model_save_path=f"models/{run.id}",
+                            model_save_path=f"{modelPath}/{run.id}",
                             verbose=2,
                         ),
             )
             run.finish()
         else:
             model.learn(total_timesteps=config["total_timesteps"])
+            
         rospy.logwarn("Training finished")
         inited = True
 
@@ -78,7 +79,7 @@ def loadModel(algorithm, modelPath):
         return PPO.load(modelPath)
     else:
         rospy.logwarn("No valid algorihtm!")
-        return 0
+        return None
     
 def startModel(algorithm, env, run, config):
     if algorithm == "DQN":
@@ -93,49 +94,46 @@ def startModel(algorithm, env, run, config):
         return PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}", n_steps = config["n_steps_before_every_PPO_update"])
     else:
         rospy.logwarn("No valid algorihtm!")
-        return 0
+        return None
 
 def evaluate(model, env, inited, num_episodes=10):
-        """
-        Evaluate a RL agent
-        :param model: (BaseRLModel object) the RL Agent
-        :param num_episodes: (int) number of episodes to evaluate it
-        :return: (float) Mean reward for the last num_episodes
-        """
-        all_episode_rewards = []
-        
-        for i in range(num_episodes):
-            episode_rewards = []
+    """
+    Evaluate a RL agent
+    :param model: (BaseRLModel object) the RL Agent
+    :param num_episodes: (int) number of episodes to evaluate it
+    :return: (float) Mean reward for the last num_episodes
+    """
+    all_episode_rewards = []
+    
+    for i in range(num_episodes):
+        episode_rewards = []
 
-            #Hack needed to enable evaluation post training :/
-            if inited:
-                obs = env.getObs()
-                inited = False
-                rospy.logwarn(str(obs))
-            else:
-                obs = env.reset()
-                
-                
-            done = False
-            while not done:
-                action, _states = model.predict(obs, deterministic=True)
-                obs, reward, done, info = env.step(action)
-                episode_rewards.append(reward)
+        #Hack needed to enable evaluation post training :/
+        if inited:
+            obs = env.getObs()
+            inited = False
+            rospy.logwarn(str(obs))
+        else:
+            obs = env.reset()
+            
+            
+        done = False
+        while not done:
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, info = env.step(action)
+            episode_rewards.append(reward)
 
-            all_episode_rewards.append(sum(episode_rewards))
+        all_episode_rewards.append(sum(episode_rewards))
 
-        mean_episode_reward = np.mean(all_episode_rewards)
-        rospy.logwarn("Mean reward: " + str(mean_episode_reward) + " Num episodes: " + str(num_episodes))
+    mean_episode_reward = np.mean(all_episode_rewards)
+    rospy.logwarn("Mean reward: " + str(mean_episode_reward) + " Num episodes: " + str(num_episodes))
 
-        return mean_episode_reward
+    return mean_episode_reward
 
 def init(algorithm):
-    rospy.init_node('example_turtlebot3_maze_qlearn',
-                    anonymous=True, log_level=rospy.WARN)
-    task_and_robot_environment_name = rospy.get_param(
-        '/turtlebot3/task_and_robot_environment_name')
-    env = StartOpenAI_ROS_Environment(
-        task_and_robot_environment_name)
+    rospy.init_node('example_turtlebot3_maze_qlearn', anonymous=True, log_level=rospy.WARN)
+    task_and_robot_environment_name = rospy.get_param('/turtlebot3/task_and_robot_environment_name')
+    env = StartOpenAI_ROS_Environment(task_and_robot_environment_name)
 
     rospack = rospkg.RosPack()
     pkg_path = rospack.get_path('drl_agent')
